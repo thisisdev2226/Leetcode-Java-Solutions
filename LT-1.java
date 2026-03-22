5411cf11e9cd605aaf79ed759dfc128080f8f206
Dev Singh <sdev72104@gmail.com>
2026-03-22 15:42:54 +0530

    Added solution for Two Sum problem using HashMap
    Implement twoSum method using HashMap for efficient lookup.

--- LT-1.java
+++ LT-1.java
@@ -1,1 +1,31 @@
+/*
+Problem: 1. Two Sum
+Link: https://leetcode.com/problems/two-sum/
+Approach: HashMap (One Pass)
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            // check if complement exists
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            // store number and index
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
